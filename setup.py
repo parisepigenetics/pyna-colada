51959ec6d16160185a603e91805f8c9064715c3b
Costas Bouyioukos <costas.bouyioukos@u-paris.fr>
2022-03-26 15:12:43 +0200

    Fix the setup file. Working version BUT FIXME the versioning!

--- setup.py
+++ setup.py
@@ -1,12 +1,58 @@
-from setuptools import setup
+import sys
+from setuptools import setup, find_packages
+import versioneer
 
-setup(name='pyna_collada',
-      version='0.1.1',
-      description='A python tool to easy work with Hi-C hic files.',
-      url='http://github.com/',
-      author='Costas Bouyioukos',
-      author_email='costas.bouyioukos@u-paris.fr',
-      license='GNU GPL 3',
-      packages=['pyna_collada'],
-      zip_safe=False
-      )
+short_description = "pyna_collada.".split("\n")[0]
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = None
+
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='pyna_collada',
+    author='Costas Bouyioukos',
+    author_email='costas.bouyioukos@u-paris.fr',
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='LGPLv3',
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_packages(),
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+    scripts=["pyna-collada"],
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # url='http://www.my_package.com',  # Website
+    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    # platforms=['Linux',
+    #            'Mac OS-X',
+    #            'Unix',
+    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    python_requires=">=3.6",          # Python version restrictions
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    zip_safe=False,
+)
